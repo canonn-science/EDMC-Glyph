3b1226a44d56e4a0bdc4567d2adbdf08615e7322
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2022-12-21 19:28:27 +0000

    Now fully functional
    But not tested with real events

--- load.py
+++ load.py
@@ -20,8 +20,6 @@ from config import appname
 
 this = sys.modules[__name__]
 
-
-
 # This could also be returned from plugin_start3()
 plugin_name = os.path.basename(os.path.dirname(__file__))
 
@@ -38,7 +36,8 @@ if not logger.hasHandlers():
 
     logger.setLevel(level)
     logger_channel = logging.StreamHandler()
-    logger_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
+    logger_formatter = logging.Formatter(
+        f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
     logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
     logger_formatter.default_msec_format = '%s.%03d'
     logger_channel.setFormatter(logger_formatter)
@@ -53,10 +52,9 @@ def plugin_start(plugin_dir):
     """
     Load this plugin into EDMC
     """
-    this.plugin_dir=plugin_dir
+    this.plugin_dir = plugin_dir
     logger.info("I am loaded! My plugin folder is {}".format(plugin_dir))
     return "Test"
-
 
 
 def plugin_app(parent):
@@ -66,8 +64,13 @@ def plugin_app(parent):
     this.frame = tk.Frame(parent)
     # By default widgets inherit the current theme's colors
 
-    this.title=tk.Label(this.frame, text="Glyph:")
-    this.status=tk.Label(this.frame, text="Waiting for scan", foreground="green")
+    this.title = tk.Label(this.frame, text="Glyph:")
+    this.status = tk.Label(
+        this.frame,
+        text="Waiting for scan",
+        foreground="green"
+    )
+
     this.title.grid(row=0, column=0, sticky="NSEW")
     this.status.grid(row=0, column=1, sticky="NSEW")
     this.title.grid_remove()
@@ -85,8 +88,8 @@ def plugin_app(parent):
 
     this.frame.columnconfigure(3, weight=1)
 
-    this.system.grid(row=2, columnspan = 2, column=0, sticky="NSEW")
-    
+    this.system.grid(row=2, columnspan=2, column=0, sticky="NSEW")
+
     this.glyph.grid(row=0, column=0, sticky="NSEW")
     this.ship.grid(row=0, column=1, sticky="NSEW")
     this.glyph_id.grid(row=1, column=0, sticky="NSEW")
@@ -100,30 +103,45 @@ def plugin_app(parent):
     this.glyph.bind('<Button-3>', rightclick)
     this.ship.bind('<Button-3>', shipclick)
     this.dismiss.bind('<Button-1>', hide)
-    this.title.bind('<Button-1>', test_show)
+    this.submit.bind('<Button-1>', glyph_submit)
+    #this.title.bind('<Button-1>', test_show)
 
-    this.inners=itertools.cycle(list(range(1,11)))
-    this.outers=itertools.cycle([
-        "1A","1B","1C","1D",
-        "2A","2B","2C","2D","2E",
-        "3A","3B",
-        "4A","4B","4C","4D","4E","4F",
+    # one through 11 inclusive
+    # should replace this by parsing the images
+    this.inners = itertools.cycle(list(range(1, 12)))
+    this.outers = itertools.cycle([
+        "1A", "1B", "1C", "1D",
+        "2A", "2B", "2C", "2D", "2E",
+        "3A", "3B",
+        "4A", "4B", "4C", "4D", "4E", "4F",
         "5A",
         "7A"
     ])
-    this.interceptors=itertools.cycle([
+    this.interceptors = itertools.cycle([
         "Cyclops",
         "Basilisk",
         "Medusa",
         "Hydra",
         "Orthrus"
     ])
-    this.interceptor=next(this.interceptors)
-    this.inner_value=next(this.inners)
-    this.outer_value=next(this.outers)
+    this.interceptor = next(this.interceptors)
+    this.inner_value = next(this.inners)
+    this.outer_value = next(this.outers)
+
     display()
     hide()
     return this.frame
+
+
+def glyph_submit(event):
+
+    url = "https://docs.google.com/forms/d/e/1FAIpQLSfv6uhfJtGuS9IizUaNO3VnX-t_AZX1DDnsRDT4Cxrj29n7Fw/formResponse?usp=pp_url"
+    params = f"&entry.1933517733={this.cmdr}&entry.896344291={this.systemName}&entry.1250600565={this.interceptor}&entry.302977208={this.id64}&entry.1507205151={this.x}&entry.554889217={this.y}&entry.743397442={this.z}&entry.245136015={this.glyph_identity}"
+
+    r = requests.get(url+params)
+
+    hide(event)
+
 
 def hide(event=None):
     this.glyph.grid_remove()
@@ -133,12 +151,17 @@ def hide(event=None):
     this.submit.grid_remove()
     this.system.grid_remove()
     this.dismiss.grid_remove()
-    #show the status 
+    # show the status
     this.title.grid()
     this.status.grid()
 
+
 def test_show(event=None):
+    edsm = get_edsm("Merope")
+    this.x, this.y, this.z = edsm.get("coords").values()
+    this.id64 = edsm.get("id64")
     show("systemName")
+
 
 def show(system):
     this.glyph.grid()
@@ -148,50 +171,72 @@ def show(system):
     this.submit.grid()
     this.system.grid()
     this.dismiss.grid()
-    #show the status 
+    # show the status
     this.title.grid_remove()
     this.status.grid_remove()
 
 
 def display():
-    
-    id=f"{this.outer_value}-{this.inner_value}"
-    gif=f"{id}.gif"
-    this.glyph_image = tk.PhotoImage(file=os.path.join(this.plugin_dir, "images", gif))
-    this.ship_image = tk.PhotoImage(file=os.path.join(this.plugin_dir, "images", f"{this.interceptor}.gif"))
-    this.glyph["image"]=this.glyph_image
-    this.ship["image"]=this.ship_image
 
-    this.glyph_id["text"]=id
-    this.ship_label["text"]=this.interceptor
+    this.glyph_identity = f"{this.outer_value}-{this.inner_value}"
+    gif = f"{this.glyph_identity}.gif"
+    this.glyph_image = tk.PhotoImage(
+        file=os.path.join(this.plugin_dir, "images", gif))
+    this.ship_image = tk.PhotoImage(file=os.path.join(
+        this.plugin_dir, "images", f"{this.interceptor}.gif"))
+    this.glyph["image"] = this.glyph_image
+    this.ship["image"] = this.ship_image
+
+    this.glyph_id["text"] = this.glyph_identity
+    this.ship_label["text"] = this.interceptor
+
 
 def set_ship(type):
     while this.interceptor != type:
-        this.interceptor=next(this.interceptors)
+        this.interceptor = next(this.interceptors)
+
 
 def leftclick(event):
-    
-    this.inner_value=next(this.inners)
-    if this.inner_value in [1,2,3]:
-        set_ship("Cyclops")
-    if this.inner_value in [4,5,6]:
-        set_ship("Medusa")    
-    if this.inner_value in [7,8,9]:
-        set_ship("Basilisk")    
-    if this.inner_value in [10]:
-        set_ship("Basilisk")    
 
+    this.inner_value = next(this.inners)
+    if this.inner_value in [1, 2, 3]:
+        set_ship("Cyclops")
+    if this.inner_value in [4, 5, 6]:
+        set_ship("Medusa")
+    if this.inner_value in [7, 8, 9]:
+        set_ship("Basilisk")
+    if this.inner_value in [10]:
+        set_ship("Basilisk")
+    if this.inner_value in [11]:
+        set_ship("Orthrus")
     display()
+
 
 def rightclick(event):
-    this.outer_value=next(this.outers)
+    this.outer_value = next(this.outers)
     display()
 
+
 def shipclick(event):
-    this.interceptor=next(this.interceptors)
+    this.interceptor = next(this.interceptors)
     display()
+
+# this should really be done with a thread
+
+
+def get_edsm(system):
+    url = f"https://www.edsm.net/api-v1/system?systemName={system}&showCoordinates=1&showId=1"
+    r = requests.get(url)
+    return r.json()
+
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     # this is what happens when you scan a goid
     if entry.get("event") == "MaterialCollected" and entry.get("Name") in ("tg_shipflightdata", "unknownshipsignature"):
+        this.cmdr = cmdr
+        this.systemName = system
+
+        edsm = get_edsm(system)
+        this.x, this.y, this.z = edsm.get("coords").values()
+        this.id64 = edsm.get("id64")
         show(system)
